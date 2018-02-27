55d16b4b98b5a2601ff0ce17a7ba10c23af19c27
Pierre Boutet <boutetpierre@yahoo.fr>
2018-02-27 14:20:30 +0100

    add Help namespace in loaded pages

--- maintenance/initWikifabClass.php
+++ maintenance/initWikifabClass.php
@@ -141,6 +141,7 @@ class InitWikifab extends Maintenance {
 		$page = str_replace ( 'Category_', 'Catégorie:', $page );
 		$page = str_replace ( 'MediaWiki_', 'Mediawiki:', $page );
 		$page = str_replace ( 'Widget_', 'Widget:', $page );
+		$page = str_replace ( 'Help_', 'Help:', $page );
 		$page = str_replace ( '_', ' ', $page );
 		$page = str_replace ( '.txt', '', $page );
 
