0b8981ed4e87d99c8d3f9db00b612326a83071b8
Julien <julien@dokit.io>
2018-03-15 12:05:44 +0100

    correction wg3dProcessor pour thumbnail dans LocalSettings.3d.php

--- LocalSettings.3d.php
+++ LocalSettings.3d.php
@@ -19,4 +19,4 @@ $wgMediaViewerExtensions['stl'] = 'mmv.3d';
 
 # tell Extension:3D how to call this thumbnail generator service
 
-$wg3dProcessor = [ '/usr/bin/xvfb-run', '-a', '-s', '-ac -screen 0 1280x1024x24' ,'lib/3d2png.js' ];
+$wg3dProcessor = [ '/usr/bin/xvfb-run', '-a', '-s', '-ac -screen 0 1280x1024x24' ,'lib/3d2png/3d2png.js' ];
