7795e1518212d89f801f58492491f5adf2756ebb
Julien <julien@dokit.io>
2018-04-17 17:34:30 +0200

    [Semantic Property Extra] - Ajouter le filtre dans l'Explorer

--- LocalSettings.wikifab.php
+++ LocalSettings.wikifab.php
@@ -131,3 +131,63 @@ $mimeMagic->addExtraTypes( "text/plain dxf" );
 $mimeMagic->addExtraTypes( "text/plain stl" );
 $mimeMagic->addExtraTypes( "application/pdf ai" );
 }
+
+$wfexploreDynamicsFilters = [
+'Page_creator' => [
+'name' => 'Page_creator',
+'type' => 'fulltext'
+],
+'Type' => [
+'name'=> 'Type',
+'translate_prefix' => 'wf-propertyvalue-type-',
+],
+'Area' => [
+'name'=> 'Area',
+'translate_prefix' => 'wf-propertyvalue-area-',
+],
+'Difficulty' => [
+'name'=> 'Difficulty',
+'translate_prefix' => 'wf-propertyvalue-difficulty-',
+],
+'Cost' => [
+'name'=> 'Cost',
+'values' =>  [
+'0-10' => '0 - 10',
+'10-50' => '10 - 50',
+'50-100' => '50 - 100',
+'100-inf' => '100 - ∞'
+]
+],
+'Language' => [
+'name'=> 'Language',
+'values' => [
+'ALL' => "all"
+],
+'translate_prefix' => 'wfexplore-language-',
+],
+'Modification_date' => [
+'name' => 'Modification_date',
+'type' => 'sort',
+'translate_prefix' => 'wfexplore-ga-last-modified'
+],
+'I_did_it' => [
+'name' => 'I_did_it',
+'type' => 'sort',
+'translate_prefix' => 'wfexplore-ididit'
+],
+'Favorites' => [
+'name' => 'Favorites',
+'type' => 'sort',
+'translate_prefix' => 'wfexplore-star'
+],
+];
+
+$GLOBALS['sespSpecialProperties'] = array(
+    '_CUSER'
+);
+
+$wfexploreCategoriesNames['Modification_date'] = wfMessage("wfexplore-filters-last-modified");
+$wfexploreCategoriesNames['Page_creator'] = wfMessage("wfexplore-filters-page-creator");
+$wfexploreCategoriesNames['Google_Analytics_Views'] = wfMessage('wfexplore-filters-views');
+$wfexploreCategoriesNames['I_did_it'] = wfMessage('wfexplore-filters-ididit');
+$wfexploreCategoriesNames['Favorites'] = wfMessage('wfexplore-filters-favorites');
