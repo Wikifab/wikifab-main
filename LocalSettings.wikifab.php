2c87e77d26280c77377e9fb062b989afa1aa9f83
Pierre Boutet <boutetpierre@yahoo.fr>
2018-08-20 15:10:48 +0200

    update directory name

--- LocalSettings.wikifab.php
+++ LocalSettings.wikifab.php
@@ -54,13 +54,13 @@ $egDraftsAutoSaveTimeout = 30;
 $wgNamespaceContentModels[NS_TALK] = 'flow-board';
 $wgNamespaceContentModels[NS_USER_TALK] = 'flow-board';
 
-$egChameleonLayoutFile= __DIR__ . '/skins/wikifabStyleModule/layout-wikifab.xml';
-$egChameleonLayoutFileSearchResult= __DIR__ . '/skins/wikifabStyleModule/layout-wikifab-search-result-int.php';
-$egChameleonLayoutFileSearchResultUserPage = __DIR__ . '/skins/wikifabStyleModule/layout-wikifab-search-result-userpage-int.php';
+$egChameleonLayoutFile= __DIR__ . '/skins/wikifab-style-module/layout-wikifab.xml';
+$egChameleonLayoutFileSearchResult= __DIR__ . '/skins/wikifab-style-module/layout-wikifab-search-result-int.php';
+$egChameleonLayoutFileSearchResultUserPage = __DIR__ . '/skins/wikifab-style-module/layout-wikifab-search-result-userpage-int.php';
 
 $egChameleonExternalStyleModules = array(
-    __DIR__ . '/skins/wikifabStyleModule/chameleon-wikifab.less' => $wgScriptPath . '/skins/wikifabStyleModule',
-    __DIR__ . '/skins/wikifabStyleModule/font-awesome-4.4.0/less/font-awesome.less' => $wgScriptPath . '/skins/wikifabStyleModule/font-awesome-4.4.0/less/font-awesome.less',
+    __DIR__ . '/skins/wikifab-style-module/chameleon-wikifab.less' => $wgScriptPath . '/skins/wikifab-style-module',
+    __DIR__ . '/skins/wikifab-style-module/font-awesome-4.4.0/less/font-awesome.less' => $wgScriptPath . '/skins/wikifab-style-module/font-awesome-4.4.0/less/font-awesome.less',
 );
 
 $wgMessagesDirs['WikifabOrg'] = __DIR__ . "/i18n"; # Location of localisation files (Tell MediaWiki to load them)
